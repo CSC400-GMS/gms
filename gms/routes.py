a474ec86753d86dea73d63a822fb0e08f75a8156
fjoao <42745876+fjoao@users.noreply.github.com>
2021-02-27 14:53:15 -0500

    Add files via upload

--- gms/routes.py
+++ gms/routes.py
@@ -1,11 +1,25 @@
 from gms import app
+from database import *
+from flask import render_template, request, redirect, url_for, flash
 
-from flask import render_template
-
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        fname = request.form['fname']
+        lname = request.form['lname']
+        email = request.form['email']
+        password = request.form['password']
+
+        sql = "INSERT into account values(?, ?, ?, ?)"
+        data = (email, password, fname, lname)
+        insert(sql,data)
+
+        flash('You have successfully registered!')
+
+        return redirect(url_for('index'))
+    else:
+        return render_template('register.html')
